a2da3e829c303268d2c92112cc307487ff277b1b
ToLa09 <landgraf-tobias@web.de>
2022-09-27 14:35:34 +0200

    replace with sum functions

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println(sum(5,10));
+        System.out.println(sumString(3,5));
+        System.out.println(getSum());
+    }
+    public static int sum(int value1, int value2) {
+        return value1 + value2;
+    }
+    public static String sumString(int value1, int value2) {
+        int result = value1 + value2;
+        return "Summe: " + result;
+    }
+    public static int getSum() {
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Hello everybody!");
+        System.out.println("Bitte erste Zahl eingeben!");
+        int value1 = scanner.nextInt();
+        System.out.println("Bitte zweite Zahl eingeben!");
+        int value2 = scanner.nextInt();
+
+        return sum(value1, value2);
     }
 }
