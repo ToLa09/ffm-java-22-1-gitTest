ef261a502d30a18dc33d05dfee1a1e9e59bd4269
ToLa09 <landgraf-tobias@web.de>
2022-10-06 11:59:20 +0200

    feat: add Shop Contructor

--- src/Shop.java
+++ src/Shop.java
@@ -2,4 +2,9 @@ public class Shop {
 
     private int clientCapacity;
     private String name;
+
+    public Shop(int clientCapacity, String name) {
+        this.clientCapacity = clientCapacity;
+        this.name = name;
+    }
 }
