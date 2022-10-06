8ebbb4e807dfb044d82d7beb06e8bf3aa765be6f
ToLa09 <landgraf-tobias@web.de>
2022-10-06 15:43:54 +0200

    feat: add variable country

--- src/Citizen.java
+++ src/Citizen.java
@@ -1,7 +1,9 @@
 public abstract class Citizen {
     private int idNumber;
+    private String country;
 
-    public Citizen(int idNumber) {
+    public Citizen(int idNumber, String country) {
         this.idNumber = idNumber;
+        this.country = country;
     }
 }
